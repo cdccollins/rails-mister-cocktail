16d9f289a9ddda4e104b992be43b626f44d4dbcd
Celia Collins <celia.collins@hotmail.com>
2018-02-23 12:15:55 +0000

    more troubleshooting

--- app/models/cocktail.rb
+++ app/models/cocktail.rb
@@ -2,6 +2,6 @@ class Cocktail < ApplicationRecord
   validates :name, presence: true, uniqueness: true
   has_many :doses, dependent: :destroy
   has_many :ingredients, through: :doses
-
+  validates :photo, presence: true
   mount_uploader :photo, PhotoUploader
 end
